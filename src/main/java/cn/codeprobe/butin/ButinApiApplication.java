ab86cb5bd9e5e94d36546d7655cb524948d9c81b
DESKTOP-LIONIDO <codeprobe@163.com>
2022-02-06 00:36:23 +0800

    v1.0 初始化 butin-api

---
+++ src/main/java/cn/codeprobe/butin/ButinApiApplication.java
@@ -0,0 +1,13 @@
+package cn.codeprobe.butin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ButinApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ButinApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/codeprobe/butin/ButinApiApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.codeprobe.butin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ButinApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
