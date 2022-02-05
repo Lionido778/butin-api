16350a9944cd0eef81f8171bba99883b2dbcd315
DESKTOP-LIONIDO <codeprobe@163.com>
2022-02-06 02:15:30 +0800

    v1.0 自定义banner,控制台打印启动成功访问提示

--- src/main/java/cn/codeprobe/butin/ButinApiApplication.java
+++ src/main/java/cn/codeprobe/butin/ButinApiApplication.java
@@ -1,13 +1,36 @@
 package cn.codeprobe.butin;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
+import org.springframework.util.StringUtils;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 @SpringBootApplication
 public class ButinApiApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(ButinApiApplication.class, args);
+    public static void main(String[] args) throws UnknownHostException {
+        Logger log = LoggerFactory.getLogger(ButinApiApplication.class);
+        ConfigurableApplicationContext context = SpringApplication.run(ButinApiApplication.class, args);
+        Environment env = context.getEnvironment();
+        String ip = InetAddress.getLocalHost().getHostAddress();
+        String port = env.getProperty("server.port");
+        String path = env.getProperty("server.servlet.context-path");
+        if (StringUtils.isEmpty(path)) {
+            path = "";
+        }
+        log.info(
+                "\n------------------------------------------------------------------\n\t" +
+                "application is running! \n\t" +
+                "Local访问网址: \t\thttp://localhost:" + port + path + "\n\t" +
+                "External访问网址: \thttp://" + ip + ":" + port + path + "\n" +
+                "------------------------------------------------------------------\n");
     }
+
 
 }
