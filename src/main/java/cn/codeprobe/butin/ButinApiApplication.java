785cfce804a35d6679df4b1d2a4472f7bb515ac0
DESKTOP-LIONIDO <codeprobe@163.com>
2022-02-09 10:52:19 +0800

    v1.0 统一日志处理,解决方案:SLF4j+Logback 自定义logback-spring.xml 适配不同的Spring Boot profile环境属性

--- src/main/java/cn/codeprobe/butin/ButinApiApplication.java
+++ src/main/java/cn/codeprobe/butin/ButinApiApplication.java
@@ -1,35 +1,38 @@
 package cn.codeprobe.butin;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import cn.hutool.core.util.StrUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.core.env.Environment;
-import org.springframework.util.StringUtils;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 
 @SpringBootApplication
+@Slf4j
 public class ButinApiApplication {
 
     public static void main(String[] args) throws UnknownHostException {
-        Logger log = LoggerFactory.getLogger(ButinApiApplication.class);
         ConfigurableApplicationContext context = SpringApplication.run(ButinApiApplication.class, args);
         Environment env = context.getEnvironment();
         String ip = InetAddress.getLocalHost().getHostAddress();
         String port = env.getProperty("server.port");
         String path = env.getProperty("server.servlet.context-path");
-        if (StringUtils.isEmpty(path)) {
-            path = "";
+        if (StrUtil.isBlank(path)) {
+            path = " ";
         }
+        log.trace("trace");
+        log.debug("debug");
+        log.warn("warn");
+        log.error("error");
         log.info(
                 "\n------------------------------------------------------------------\n\t" +
-                "application is running! \n\t" +
-                "Local访问网址: \t\thttp://localhost:" + port + path + "\n\t" +
-                "External访问网址: \thttp://" + ip + ":" + port + path + "\n" +
-                "------------------------------------------------------------------\n");
+                        "application is running! \n\t" +
+                        "Local访问网址: \t\thttp://localhost:" + port + path + "\n\t" +
+                        "External访问网址: \thttp://" + ip + ":" + port + path + "\n" +
+                        "------------------------------------------------------------------\n");
     }
 
 
