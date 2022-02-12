adcb3567a6ae1bf89daf950fdad5ede5f4e684d6
DESKTOP-LIONIDO <codeprobe@163.com>
2022-02-12 22:41:20 +0800

    v1.0 解决lombok注解@Data使用在继承类上时出现警告

--- src/main/java/cn/codeprobe/butin/exception/ButinException.java
+++ src/main/java/cn/codeprobe/butin/exception/ButinException.java
@@ -2,11 +2,13 @@ package cn.codeprobe.butin.exception;
 
 import cn.codeprobe.butin.exception.response.Status_Error;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 /**
  * Created by Lionido on 12/2/2022
  * 自定义异常类
  */
+@EqualsAndHashCode(callSuper = true)
 @Data
 public class ButinException extends RuntimeException {
 
